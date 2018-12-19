bee707029d31d80eb806ebe4d686785add194d05
hoshianaaa <40942409+hoshianaaa@users.noreply.github.com>
2018-12-19 13:26:48 +0900

    Update main.c

--- arduino/main.c
+++ arduino/main.c
@@ -1,40 +1,74 @@
+volatile int count_h = 0;
+volatile int count_m = 0;
+int sw[4] = {3,4,5,6};
+int start_sw = 0;
+int stage = 0;
+
+
+int value[4];
+int d_value[4];
+
+long start_time = 0;
+long set_time = 0;
+long long second(int ,int );
+
 void setup() {
   // put your setup code here, to run once:
-  attachiInterrupt(1,timerstop(),FALLING);
+  Serial.begin(9600);
+  attachInterrupt(2,func_a,FALLING);
+  attachInterrupt(3,func_b,FALLING);
+  attachInterrupt(4,func_c,FALLING);
+  attachInterrupt(5,func_d,FALLING);
+  attachInterrupt(0,func_e,FALLING);
+  
+}
+void loop() {
+    
+    Serial.print(abs(count_h%25));
+    Serial.print(":");
+    Serial.println(count_m%60);
+    second(abs(count_h%25),count_m%60);
+
+    while(stage == 2)
+    {
+      start_time = millis()/1000;//stage2に入った時間を秒に直して入力する
+      stage = 3; 
+    }
+
+    while(stage == 3){
+      set_time = second(count_h%25,count_m%61);
+      Serial.println(set_time - millis()/1000 - start_time); 
+    }
+    
+    
+  }
+
+
+void func_a(){ count_h ++;}
+void func_b(){ count_h --;}
+void func_c(){ count_m ++;}
+void func_d(){ count_m --;}
+void func_e(){ stage = 2;}
+
+
+
+//何時何分かを入力するとそれが何秒か表示
+long long second(int hour,int minitue){
+  return minitue*60+hour*3600;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  while (stage == 0) { servo_up();stage = 1;}
-  while (stage == 1) { Serial.println("00:00");stage = 2; }
-  while (stage == 2) { 
-    if(sw_start == LOW || set_time != 0)stage = 3;
-    set_h = set_count(sw1,sw2);
-    set_m = set_count(sw3,sw4);
-    Serial.print(set_h);
-    Serial.print(":");
-    Serial.print(set_m);
-  }
-  while (stage == 3){
-    if((set_h - now_h) == 0)stage = 4;
-    Serial.print(set_h - nowh);
-    Serial.print(":");
-    Serial.print(set_m - now_m);
-    Serial.print(":");
-    Serial.println(set_s - now_s);
-  }
-  while (stage == 4){
-    if((set_m - now_m) == 0)stage = 5;
-    Serial.print("00:");
-    Serial.print(set_m - now_m);
-    Serial.print(":");
-    Serial.println(set_s - now_s);
-  }
-  while (stage == 5){
-    if((set_s < now_s)servo_down();stage = 0;
-      Serial.print("00:00:");
-      Serial.println(set_s - now_s);
-  }
-  
-  
+//時間を秒単位で入力すると、h,m,sの部分だけを抽出する
+long hour(long long second){
+  return second/3600;
 }
+
+long mini(long long second){
+  return (second-hour(second)*3600)/60;
+}
+
+long sec(long long second){
+  return second-hour(second)*3600-mini(second)*60;
+}
+
+
+ 
